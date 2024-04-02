c3c8b388c30ee72f6de06d68358352c562bb146b
Francois Daoust <fd@tidoust.net>
2024-04-02 10:54:41 +0200

    Retrieve env variables from GitHub through "gh" CLI
    There is no GraphQL endpoint to retrieve variables but the REST API can be used
    in practice, and that's precisely what the `gh` CLI does. Using it to retrieve
    variables means it's no longer needed to repeat variables locally in a
    `config.json` file. This is useful for local execution.
    
    Note: Secrets are still needed! They cannot be retrieved.

--- tools/lib/envkeys.mjs
+++ tools/lib/envkeys.mjs
@@ -1,6 +1,8 @@
 import path from 'path';
+import { execSync } from 'child_process';
 
 let config = null;
+let repoConfig = null;
 
 /**
  * Retrieve the requested variable from the environment or from the
@@ -10,9 +12,13 @@ let config = null;
  * value was provided.
  */
 export async function getEnvKey(key, defaultValue, json) {
+  // If the environment explicitly defines the key, that's good, let's use it!
   if (Object.hasOwn(process.env, key)) {
     return json ? JSON.parse(process.env[key]) : process.env[key];
   }
+
+  // Initialize the local configuration from the config.json file, if defined
+  // (note that is done only once)
   try {
     if (!config) {
       const configFileUrl = 'file:///' +
@@ -26,13 +32,27 @@ export async function getEnvKey(key, defaultValue, json) {
   }
   catch {
   }
-  finally {
-    if (config && Object.hasOwn(config, key)) {
-      return config[key];
+
+  // Retrieve variables from the GitHub repo directly through the "gh" CLI.
+  try {
+    const repoVariablesStr = execSync(`gh variable list --json name,value`);
+    const repoVariables = JSON.parse(repoVariablesStr);
+    repoConfig = {};
+    for (const variable of repoVariables) {
+      repoConfig[variable.name] = variable.value;
     }
-    else if (defaultValue !== undefined) {
-      return defaultValue;
-    }
-    throw new Error(`No ${key} found in environment of config file.`);
   }
+  catch {
+  }
+
+  if (config && Object.hasOwn(config, key)) {
+    return config[key];
+  }
+  else if (repoConfig && Object.hasOwn(repoConfig, key)) {
+    return repoConfig[key];
+  }
+  else if (defaultValue !== undefined) {
+    return defaultValue;
+  }
+  throw new Error(`No ${key} found in environment of config file.`);
 }
