5d98f08314eb9fa53e13e1f4bbb3b65bdca27839
Francois Daoust <fd@tidoust.net>
2024-04-04 16:22:23 +0200

    Enable programmatic overrides of env keys
    This will be useful to run tests

--- tools/lib/envkeys.mjs
+++ tools/lib/envkeys.mjs
@@ -1,7 +1,8 @@
 import path from 'path';
 import { execSync } from 'child_process';
 
-let config = null;
+let localConfig = {};
+let fileConfig = null;
 let repoConfig = null;
 
 /**
@@ -12,6 +13,12 @@ let repoConfig = null;
  * value was provided.
  */
 export async function getEnvKey(key, defaultValue, json) {
+  // If the environment key was set through a call to setEnvKey, let's use it!
+  // (typically used in tests)
+  if (Object.hasOwn(localConfig, key)) {
+    return json ? JSON.parse(localConfig[key]) : localConfig[key];
+  }
+
   // If the environment explicitly defines the key, that's good, let's use it!
   if (Object.hasOwn(process.env, key)) {
     return json ? JSON.parse(process.env[key]) : process.env[key];
@@ -19,42 +26,52 @@ export async function getEnvKey(key, defaultValue, json) {
 
   // Initialize the local configuration from the config.json file, if defined
   // (note that is done only once)
-  try {
-    if (!config) {
+  if (!fileConfig) {
+    fileConfig = {};
+    try {
       const configFileUrl = 'file:///' +
         path.join(process.cwd(), 'config.json').replace(/\\/g, '/');
       const { default: env } = await import(
         configFileUrl,
         { assert: { type: 'json' } }
       );
-      config = env;
+      fileConfig = env;
+    }
+    catch {
     }
   }
-  catch {
+  if (Object.hasOwn(fileConfig, key)) {
+    return fileConfig[key];
   }
 
   // Retrieve variables from the GitHub repo directly through the "gh" CLI.
-  try {
-    if (!repoConfig) {
+  if (!repoConfig) {
+    repoConfig = {};
+    try {
       const repoVariablesStr = execSync(`gh variable list --json name,value`);
       const repoVariables = JSON.parse(repoVariablesStr);
-      repoConfig = {};
       for (const variable of repoVariables) {
         repoConfig[variable.name] = variable.value;
       }
     }
+    catch {
+    }
   }
-  catch {
-  }
-
-  if (config && Object.hasOwn(config, key)) {
-    return config[key];
-  }
-  else if (repoConfig && Object.hasOwn(repoConfig, key)) {
+  if (Object.hasOwn(repoConfig, key)) {
     return repoConfig[key];
   }
-  else if (defaultValue !== undefined) {
+
+  if (defaultValue !== undefined) {
     return defaultValue;
   }
   throw new Error(`No ${key} found in environment of config file.`);
+}
+
+
+/**
+ * Set the environment variable for next getEnvKey calls.
+ * (useful in tests)
+ */
+export async function setEnvKey(key, value, json) {
+  localConfig[key] = json ? JSON.stringify(value) : value;
 }
