256a1031113897c8e9095536e0f0b59e53783a8f
Francois Daoust <fd@tidoust.net>
2024-06-14 10:50:06 +0200

    Add functional tests for group meeting highlights
    See description in #137.
    
    A couple of tests already pass. They must not start failing once the feature
    gets implemented. That should be a no-brainer for the second one, because the
    code that merges meetings of a group already proceeds on a session by session
    basis and cannot merge meetings that belong to two different sessions.

---
+++ test/data/group-highlight.mjs
@@ -0,0 +1,56 @@
+export default {
+  allowMultipleMeetings: true,
+  description: 'meeting: Group meeting highlights, timezone: Etc/UTC, type: groups',
+
+  days: [
+    'Monday (2042-02-10)'
+  ],
+
+  slots: [
+    '9:00 - 11:00',
+    '11:00 - 13:00',
+    '14:00 - 16:00'
+  ],
+
+  rooms: [
+    'Room 1',
+    'Room 2'
+  ],
+
+  sessions: [
+    {
+      number: 1,
+      title: 'WICG: Digital Credentials API',
+      room: 'Room 1',
+      meeting: 'Monday, 11:00'
+    },
+
+    {
+      number: 2,
+      title: 'Web Platform Incubator CG',
+      room: 'Room 1',
+      meeting: 'Monday, 9:00; Monday, 14:00'
+    },
+
+    {
+      number: 3,
+      title: 'WAI-Engage: Web Accessibility Community Group',
+      room: 'Room 2',
+      meeting: 'Monday, 9:00'
+    },
+
+    {
+      number: 4,
+      title: 'Second Screen WG > OSP',
+      room: 'Room 2',
+      meeting: 'Monday, 11:00'
+    },
+
+    {
+      number: 5,
+      title: 'Second Screen WG & Media WG Joint Meeting: Media streaming',
+      room: 'Room 2',
+      meeting: 'Monday, 14:00'
+    }
+  ]
+}
