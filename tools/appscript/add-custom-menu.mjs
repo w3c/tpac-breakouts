a39364524b62fce2cd9ed1a1fe399aa84937ceff
Ian Jacobs <ij@w3.org>
2025-03-13 11:27:38 -0500

    more menu title changes

--- tools/appscript/add-custom-menu.mjs
+++ tools/appscript/add-custom-menu.mjs
@@ -14,12 +14,12 @@ export default function () {
         .addItem('Validate metadata and propose a new schedule', 'proposeGrid')
         .addItem('Refresh table view of schedule', 'generateGrid')
     )
-    .addItem('Publish the grid and calendar', 'exportGrid')
+    .addItem('Publish the schedule and calendar', 'exportGrid')
     .addSeparator()
     .addSubMenu(
       SpreadsheetApp.getUi()
         .createMenu('Advanced')
-        .addItem('Retrieve latest published grid (from GitHub)', 'importGrid')
+        .addItem('Retrieve latest published schedule (from GitHub)', 'importGrid')
         .addItem('Fetch event info, rooms, days, slots (from GitHub)', 'importMetadata')
         /*.addItem('Export event info, rooms, days, slots to GitHub', 'exportMetadata')*/
         .addItem('Dump event data as JSON', 'exportEventData')
