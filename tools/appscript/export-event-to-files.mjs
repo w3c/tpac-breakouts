db86372a6c1f889b21c4ab6141b5dc0d3e09d090
François Daoust <fd@w3.org>
2025-07-08 11:37:30 +0200

    Add export view that mimics info in W3C admin interface (#267)
    To ask TPAC registrants about what groups they plan to attend, the list of
    group meetings needs to be added to the registration form in the W3C admin
    interface.
    
    This process could be automated in the long term. In the short term, this
    adds a new HTML file to the "Export event to files" advanced menu that
    mimics what the resulting table should look like in the W3C admin interface:
    https://www.w3.org/admin/tpac-meetings/list

--- tools/appscript/export-event-to-files.mjs
+++ tools/appscript/export-event-to-files.mjs
@@ -1,7 +1,9 @@
 import { getProject } from './lib/project.mjs';
 import reportError from './lib/report-error.mjs';
 import { fetchMapping } from './lib/w3cid-map.mjs';
-import { convertProjectToHTML } from '../common/project2html.mjs';
+import {
+  convertProjectToHTML,
+  convertProjectToRegistrationHTML } from '../common/project2html.mjs';
 
 
 export default function () {
@@ -27,10 +29,19 @@ async function exportEventToFiles(spreadsheet) {
     const html = await convertProjectToHTML(project, options);
     console.log('Convert to HTML... done');
 
+    console.log('Convert to registration table...');
+    const registrationHtml = await convertProjectToRegistrationHTML(project);
+    console.log('Convert to registration table... done');
+
     console.log('Create data URL for the HTML export...');
     const htmlBase64 = Utilities.base64Encode(html, Utilities.Charset.UTF_8);
     const htmlDataUrl = `data:text/html;charset=UTF8;base64,${htmlBase64}`;
     console.log('Create data URL for the HTML export... done');
+
+    console.log('Create data URL for the registration table...');
+    const registrationBase64 = Utilities.base64Encode(registrationHtml, Utilities.Charset.UTF_8);
+    const registrationDataUrl = `data:text/html;charset=UTF8;base64,${registrationBase64}`;
+    console.log('Create data URL for the registration table... done');
 
     console.log('Create data URL for the JSON export...');
     const jsonBase64 = Utilities.base64Encode(
@@ -48,12 +59,17 @@ async function exportEventToFiles(spreadsheet) {
         <ul>
           <li>
             <a href="${htmlDataUrl}" download="event.html">
-              Download event schedule as an HTML file
+              Event schedule (HTML file)
+            </a>
+          </li>
+          <li>
+            <a href="${registrationDataUrl}" download="registration.html">
+              Registration form view (HTML file)
             </a>
           </li>
           <li>
             <a href="${jsonDataUrl}" download="event.json">
-              Download event data as a JSON file
+              Event data (JSON file)
             </a>
           </li>
         </ul>
